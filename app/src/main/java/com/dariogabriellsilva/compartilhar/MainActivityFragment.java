7fdd29e14b2fbc4fc47e8e37d949cfd0642d65bd
Dario <dariogabriel113@gmail.com>
2019-03-26 10:09:32 -0300

    Cria fragment.

---
+++ app/src/main/java/com/dariogabriellsilva/compartilhar/MainActivityFragment.java
@@ -0,0 +1,20 @@
+package com.dariogabriellsilva.compartilhar;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class MainActivityFragment extends Fragment {
+    public MainActivityFragment() {
+
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_main, container, false);
+    }
+}
